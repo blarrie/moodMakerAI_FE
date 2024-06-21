d251ad589935e9ec0a260fa09d197759d3819fc9
clara.aw.2021 <clara.aw.2021@scis.smu.edu.sg>
2024-06-22 02:14:21 +0800

    add scaffolding and vuetify

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// remove this line or edit this file for css
+import './assets/main.css'
+
+import { createApp } from 'vue'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// Components
+import App from './App.vue'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App).use(vuetify).mount('#app')
