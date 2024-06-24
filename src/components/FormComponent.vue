48addd932672818bcfd5a8a094629d72a0443f32
clara.aw.2021 <clara.aw.2021@scis.smu.edu.sg>
2024-06-24 20:46:53 +0800

    create form component

---
+++ src/components/FormComponent.vue
@@ -0,0 +1,46 @@
+<script>
+import "@mdi/font/css/materialdesignicons.css"; // Ensure you are using css-loader
+
+export default {
+  methods: {
+    submitForm() {
+      console.log("Enter key pressed!");
+    },
+  },
+};
+</script>
+
+<template>
+  <v-form @submit.prevent="submitForm">
+    <v-file-input
+      accept="video/*"
+      label="Input Video File Here"
+      class="input"
+      icon="mdi-paperclip"
+    >
+    </v-file-input>
+
+    <v-btn
+      rounded="sm"
+      variant="outlined"
+      color="red"
+      append-icon="mdi-music-box-multiple-outline"
+      type="submit"
+    >
+      Find Music!
+    </v-btn>
+
+  </v-form>
+
+
+</template>
+
+<style>
+.input {
+  width: 500px;
+}
+p {
+  color: white;
+  font-size: larger;
+}
+</style>
